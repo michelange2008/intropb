ed452ae1afb54754591721c05cc16bf73df087da
Michel BOUY <michelange@wanadoo.fr>
2025-06-12 15:00:25 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Choix panse-bêtes</title>
+    <link rel="stylesheet" href="intropb.css" />
+    <link href="https://fonts.cdnfonts.com/css/roboto" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="fond">
+        <div class="cartouche">
+            <img src="img/otoveil.jpeg" alt="logo" class="logo">
+            <div class="titre">
+                <h1>Deux versions</h1>
+                <h1>de</h1>
+                <h1>Panse-bêtes</h1>
+            </div>
+        </div>
+        <div class="main">
+            <p>Il y a aujourd'hui deux versions de panse-bêtes:</p>
+            <ol>
+                <li>
+                    La version originale, créée lors du casdar Otoveil
+                    et qui est utilisable sur toutes les espèces sauf les vaches laitières.
+                </li>
+                <li>
+                    Une version améliorée pour les vaches laitières qui a été réalisées
+                    par le projet ACSA en Bourgogne Franche-Comté.
+                </li>
+            </ol>
+            <p>Nous espérons à terme pouvoir faire une version améliorée de Panse-Bêtes
+                pour toutes les espèces.
+            </p>
+            <h3>
+                Dans l'immédiat, en cliquant sur l'atelier correspondant à votre élevage,
+                vous serez redirigés vers la version adaptée.
+            </h3>
+            <div class="liste-vignettes">
+                <a href="https://neo.panse-betes.fr">
+                    <img class="vignettes" src="img/VL.svg" alt="vache laitières" title="Vaches laitières">
+                </a>
+                <a href="https://old.panse-betes.fr">
+                    <img class="vignettes" src="img/VA.svg" alt="vache allaitantes" title="Vaches allaitantes">
+                </a>
+                <a href="https://old.panse-betes.fr">
+                    <img class="vignettes" src="img/CP.svg" alt="chèvres laitières" title="Chèvres laitières">
+                </a>
+                <a href="https://old.panse-betes.fr">
+                    <img class="vignettes" src="img/OA.svg" alt="brebis allaitantes" title="Brebis allaitantes">
+                </a>
+                <a href="https://old.panse-betes.fr">
+                    <img class="vignettes" src="img/OL.svg" alt="Brebis laitières" title="Brebis laitières">
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+<div></div>
